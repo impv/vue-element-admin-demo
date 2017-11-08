f30801b063286b93631169986174544ebaa33892
pocka <pocka.moe@gmail.com>
2017-11-08 13:08:54 +0900

    Add webpack config and tasks runs them

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const path = require('path')
+
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
+
+const babelOptions = {
+  presets: [
+    ['env', {
+      targets: {
+        browsers: 'last 2 versions'
+      },
+      modules: false
+    }]
+  ],
+  plugins: ['transform-object-rest-spread']
+}
+
+const envPlugin = new webpack.DefinePlugin({
+  'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+})
+
+const baseConfig = {
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            js: {
+              loader: 'babel-loader',
+              options: babelOptions
+            }
+          },
+          extractCSS: process.env.NODE_ENV === 'production'
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: babelOptions
+        }
+      }
+    ]
+  },
+  plugins: [
+    envPlugin
+  ],
+  resolve: {
+    extensions: ['.vue', '.js'],
+    alias: {
+      vue$: 'vue/dist/vue.esm.js'
+    }
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = merge(baseConfig, {
+    entry: ['babel-polyfill', './src/index.js'],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: 'css-loader'
+          })
+        },
+        {
+          test: /\.(ttf|woff|eot|woff2)$/,
+          loaders: 'file-loader'
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('style.css'),
+      new UglifyPlugin()
+    ]
+  })
+} else {
+  module.exports = merge(baseConfig, {
+    entry: ['./src/index.js'],
+    devServer: {
+      historyApiFallback: true,
+      contentBase: 'public'
+    },
+    devtool: '#eval-source-map'
+  })
+}
