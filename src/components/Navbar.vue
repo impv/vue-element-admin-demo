019d38c58e9fbeb8d1d5a8df65093677433e7803
pocka.moe@gmail.com <pocka.moe@gmail.com>
2017-11-09 18:38:59 +0900

    Implement Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<template>
+  <el-menu
+    class="menu"
+    background-color="#1F218B"
+    text-color="#fff"
+  >
+    <el-menu-item index="logout">Logout</el-menu-item>
+  </el-menu>
+</template>
+
+<style scoped>
+.menu {
+  height: 100vh;
+}
+</style>
