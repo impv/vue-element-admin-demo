412553e4545eb743bf12691c99c9481974ad9193
pocka.moe@gmail.com <pocka.moe@gmail.com>
2017-11-10 13:03:57 +0900

    Add validator

--- src/types/User.ts
+++ src/types/User.ts
@@ -2,3 +2,23 @@ export default interface User {
   name: string
   password: string
 }
+
+export const isValid = (user: User): boolean => {
+  return !validateName(user) && !validatePassword(user)
+}
+
+export const validateName = (user: User): string => {
+  if (!user.name) {
+    return 'Name is required'
+  }
+
+  return ''
+}
+
+export const validatePassword = (user: User): string => {
+  if (!user.password) {
+    return 'Password is required'
+  }
+
+  return ''
+}
