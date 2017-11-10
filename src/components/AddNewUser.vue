f242b80bb5bd5427ddddcbf83bdd86be68bef1f2
pocka.moe@gmail.com <pocka.moe@gmail.com>
2017-11-10 13:05:26 +0900

    Add AddNewUser component

---
+++ src/components/AddNewUser.vue
@@ -0,0 +1,76 @@
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+import User, { isValid, validateName, validatePassword } from '../types/User'
+
+import { AddUser } from '../store/modules/users'
+
+import FormItem from './FormItem.vue'
+
+@Component({
+  components: { FormItem }
+})
+export default class AddNewUser extends Vue {
+  private user: User = {
+    name: '',
+    password: ''
+  }
+
+  private errors: {[key: string]: string } = {
+    name: '',
+    password: ''
+  }
+
+  private get isValid(): boolean {
+    return isValid(this.user)
+  }
+
+  private validateName() {
+    this.errors.name = validateName(this.user)
+  }
+
+  private validatePassword() {
+    this.errors.password = validatePassword(this.user)
+  }
+
+  private add() {
+    this.$store.dispatch(AddUser, { user: this.user } as AddUser).then(() => {
+      this.$router.push('/users')
+    }).catch(err => {
+      this.$message.error(err.message)
+    })
+  }
+}
+</script>
+
+<template>
+  <el-main>
+    <el-form>
+      <FormItem label="Name" :error="errors.name">
+        <el-input
+          v-model="user.name"
+          @input="validateName"
+          @blur="validateName"
+          type="text"
+        />
+      </FormItem>
+      <FormItem label="Password" :error="errors.password">
+        <el-input
+          v-model="user.password"
+          @input="validatePassword"
+          @blur="validatePassword"
+          type="password"
+        />
+      </FormItem>
+      <FormItem>
+        <el-button type="primary" @click="add" :disabled="!isValid">
+          Register
+        </el-button>
+      </FormItem>
+    </el-form>
+  </el-main>
+</template>
+
+<style scoped>
+</style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,13 @@
 import { RouteConfig } from 'vue-router'
 
+import AddNewUser from '../components/AddNewUser.vue'
 import Dashboard from '../components/Dashboard.vue'
 import Users from '../components/Users.vue'
 
 const routes: RouteConfig[] = [
-  {path: '/', component: Dashboard },
-  {path: '/users', component: Users }
+  { path: '/', component: Dashboard },
+  { path: '/users', component: Users },
+  { path: '/users/new', component: AddNewUser }
 ]
 
 export default routes
