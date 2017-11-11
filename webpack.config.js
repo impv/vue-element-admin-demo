9751050273bd0f75d1aaee4e378b83fcc41a5f69
pocka <pocka.moe@gmail.com>
2017-11-11 14:12:54 +0900

    Make webpack being able to compile scss

--- webpack.config.js
+++ webpack.config.js
@@ -65,10 +65,10 @@ if (process.env.NODE_ENV === 'production') {
     module: {
       rules: [
         {
-          test: /\.css$/,
+          test: /\.s?css$/,
           use: ExtractTextPlugin.extract({
             fallback: 'style-loader',
-            use: 'css-loader'
+            use: 'css-loader!sass-loader'
           })
         },
         {
@@ -88,8 +88,8 @@ if (process.env.NODE_ENV === 'production') {
     module: {
       rules: [
         {
-          test: /\.css$/,
-          loaders: 'style-loader!css-loader'
+          test: /\.s?css$/,
+          loaders: 'style-loader!css-loader!sass-loader'
         },
         {
           test: /\.ttf$/,
