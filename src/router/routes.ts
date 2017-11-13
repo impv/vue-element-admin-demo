4ebd87138435cbfb00a2844fa4d5f8ddae092860
pocka.moe@gmail.com <pocka.moe@gmail.com>
2017-11-13 15:53:42 +0900

    Add user edit page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,12 +2,14 @@ import { RouteConfig } from 'vue-router'
 
 import AddNewUser from '../components/AddNewUser/index.vue'
 import Dashboard from '../components/Dashboard.vue'
+import EditUser from '../components/EditUser.vue'
 import Users from '../components/Users.vue'
 
 const routes: RouteConfig[] = [
   { path: '/', component: Dashboard },
   { path: '/users', component: Users },
-  { path: '/users/new', component: AddNewUser }
+  { path: '/users/new', component: AddNewUser },
+  { path: '/users/edit/:name', component: EditUser, props: true }
 ]
 
 export default routes
